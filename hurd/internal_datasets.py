b624e73d9fe47c765a8ba04e0f0e99f273a9e035
Brooke <brookekelseyryan@gmail.com>
2023-03-21 10:36:12 -0700

    updated internal_datasets to include EV dataset

--- hurd/internal_datasets.py
+++ hurd/internal_datasets.py
@@ -7,10 +7,12 @@ def load_c13k_data(fb_filter="only_fb"):
 
     fb_filter = str(fb_filter)
 
-    if fb_filter not in ["only_fb", "no_fb", "None"]:
-        raise ValueError("fb_filter must be 'only_fb', 'no_fb', or None")
-
-    fn = "data_c13k_no_amb_" + fb_filter
+    if fb_filter in ["only_fb", "no_fb", "None"]:
+        fn = "data_c13k_no_amb_" + fb_filter
+    elif fb_filter in ["amb_no_fb_EV_B_highest_target", "EV_B_highest_target"]:
+        fn = "data_c13k_" + fb_filter
+    else:
+        raise ValueError("fb_filter must be 'only_fb', 'no_fb', 'EV_B_highest_target', or None")
 
     real_path = os.path.realpath(__file__)
     dir_path = os.path.dirname(real_path)
